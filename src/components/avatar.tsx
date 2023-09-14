86fca7f27d437899028b80e48d6cb80d3de2b674
AndrewZuo01 <andrew.zuo@wustl.edu>
2023-09-14 17:07:51 +0800

    finish receive and send message

--- src/components/avatar.tsx
+++ src/components/avatar.tsx
@@ -1,65 +1,74 @@
 import { Image, StyleSheet, Text, View } from "react-native";
 import images from "./defaultAvatarHelper";
+import LinearGradient from "react-native-linear-gradient";
 
-const Avatar = ({ item }:{item:any}) => {
+const Avatar = ({ item }: { item: any }) => {
   if (!item) {
     return null; // Return null or a placeholder component if data is undefined
   }
-  if(item.faceURL==="" && item.nickname){
-    
-    return(
-        <View >
-            <View style = {styles.textImage}>
-                <Text style = {styles.textImageText}>{item.nickname.charAt(0).toUpperCase()}</Text>
-            </View>
-        </View>
+  if (item.faceURL === "" && item.nickname) {
+
+    return (
+      <LinearGradient
+        colors={['#EAF27E', '#00D292']}
+        start={{ x: 0, y: 0 }}
+        end={{ x: 1, y: 1 }}
+        style={styles.textImage}
+      >
+
+        <Text style={styles.textImageText}>{item.nickname.charAt(0).toUpperCase()}</Text>
+      </LinearGradient>
     );
-  }else if(item.faceURL==="" && item.showName){
-    return(<View >
-        <View style = {styles.textImage}>
-            <Text style = {styles.textImageText}>{item.showName.charAt(0).toUpperCase()}</Text>
-        </View>
-    </View>);
-  } 
-  else if(item.faceURL==="ic_avatar_01"){
-    return(
-        <Image style={styles.avatar} source={images.ic_avatar_01} />
+  } else if (item.faceURL === "" && item.showName) {
+    return (
+      <LinearGradient
+        colors={['#EAF27E', '#00D292']}
+        start={{ x: 0, y: 0 }}
+        end={{ x: 1, y: 1 }}
+        style={styles.textImage}
+      >
+        <Text style={styles.textImageText}>{item.showName.charAt(0).toUpperCase()}</Text>
+      </LinearGradient>);
+  }
+  else if (item.faceURL === "ic_avatar_01") {
+    return (
+      <Image style={styles.avatar} source={images.ic_avatar_01} />
     );
-  }else if(item.faceURL==="ic_avatar_02"){
-    return(
-        <Image style={styles.avatar} source={images.ic_avatar_02} />
+  } else if (item.faceURL === "ic_avatar_02") {
+    return (
+      <Image style={styles.avatar} source={images.ic_avatar_02} />
     );
-  }else if(item.faceURL==="ic_avatar_03"){
-    return(
-        <Image style={styles.avatar} source={images.ic_avatar_03} />
+  } else if (item.faceURL === "ic_avatar_03") {
+    return (
+      <Image style={styles.avatar} source={images.ic_avatar_03} />
     );
-  }else if(item.faceURL==="ic_avatar_04"){
-    return(
-        <Image style={styles.avatar} source={images.ic_avatar_04} />
+  } else if (item.faceURL === "ic_avatar_04") {
+    return (
+      <Image style={styles.avatar} source={images.ic_avatar_04} />
     );
-  }else if(item.faceURL==="ic_avatar_05"){
-    return(
-        <Image style={styles.avatar} source={images.ic_avatar_05} />
+  } else if (item.faceURL === "ic_avatar_05") {
+    return (
+      <Image style={styles.avatar} source={images.ic_avatar_05} />
     );
-  }else if(item.faceURL==="ic_avatar_06"){
-    return(
-        <Image style={styles.avatar} source={images.ic_avatar_06} />
+  } else if (item.faceURL === "ic_avatar_06") {
+    return (
+      <Image style={styles.avatar} source={images.ic_avatar_06} />
     );
-  }else if(item.faceURL==="New Friend"){
-    return(
-        <Image style={styles.avatar} source={images.newFriend} />
+  } else if (item.faceURL === "New Friend") {
+    return (
+      <Image style={styles.avatar} source={images.newFriend} />
     );
-  }else if(item.faceURL==="New Group"){
-    return(
-        <Image style={styles.avatar} source={images.newGroup} />
+  } else if (item.faceURL === "New Group") {
+    return (
+      <Image style={styles.avatar} source={images.newGroup} />
     );
-  }else if(item.faceURL==="My Groups"){
-    return(
-        <Image style={styles.avatar} source={images.myGroups} />
+  } else if (item.faceURL === "My Groups") {
+    return (
+      <Image style={styles.avatar} source={images.myGroups} />
     );
   }
   return (
-      <Image style={styles.avatar} source={{uri: item.faceURL}} />
+    <Image style={styles.avatar} source={{ uri: item.faceURL }} />
   );
 };
 
@@ -77,18 +86,18 @@ const styles = StyleSheet.create({
     borderRadius: 25,
     marginRight: 10,
   },
-  textImage:{
+  textImage: {
     height: 50,
     width: 50,
     borderRadius: 25,
     marginRight: 10,
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor:"blue"
+    backgroundColor: "blue"
   },
-  textImageText:{
-    color:"white",
-    textAlign:'center'
+  textImageText: {
+    color: "white",
+    textAlign: 'center'
   }
 });
 
